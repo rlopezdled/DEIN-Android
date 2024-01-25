77e1e5d83a28742886e4fea926869d6b8b88f550
Rubén <rlopezdled@educacion.navarra.es>
2024-01-25 13:28:51 +0100

    Añadidos archivos de themes.xml y colors.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.temasderubenlopezdavalillo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.temasderubenlopezdavalillo"
